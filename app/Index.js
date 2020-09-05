a232115daf5102c715b5e7195045128b58379cc0
NB-famous <ncbadoles@gmail.com>
2020-09-05 13:05:38 -0400

    my first commit

---
+++ app/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function ExampleComponent(){
+    return (
+        <div>
+            <h1> This is our app</h1>
+            <p> The Sky is blue</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<ExampleComponent/>, document.querySelector("#app"))
