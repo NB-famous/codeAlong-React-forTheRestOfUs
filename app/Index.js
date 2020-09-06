2d1d5557aae8e6c6bbec3dc0f8391aa22762e024
NB-famous <ncbadoles@gmail.com>
2020-09-05 20:01:55 -0400

    Added components and css file

--- app/Index.js
+++ app/Index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function ExampleComponent(){
+/////// Components //////////
+import Header from './components/Header'
+import HomeGuest from './components/HomeGuest'
+import Footer from './components/Footer'
+
+function Main(){
     return (
-        <div>
-            <h1> This is our app</h1>
-            <p> The Sky is blue</p>
-        </div>
+        <>
+            <Header />
+            <HomeGuest />
+            <Footer />
+        </>
     )
 }
 
 
-ReactDOM.render(<ExampleComponent/>, document.querySelector("#app"))
+ReactDOM.render(<Main/>, document.querySelector("#app"))
+
+if(module.hot) {
+    module.hot.accept()
+}
